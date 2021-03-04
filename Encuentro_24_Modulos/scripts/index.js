7c8d23c01e3544c5a08d60b3acf8b2043a69f718
DWFEonline3 <dwfe.online.3@gmail.com>
2021-03-03 19:55:53 -0500

    Ejercicios encuentro 24 Modulos

---
+++ Encuentro_24_Modulos/scripts/index.js
@@ -0,0 +1,22 @@
+/*let msg2 = "hola 2";
+if(5>2){
+    console.log(msg2);
+}
+console.log(msg2); 
+
+if(5>2){
+    var msg3 = "hola 3";
+    console.log(msg3);
+}
+console.log(msg3);
+
+if(5>2){
+    let msg = "hola";
+    console.log(msg);
+}
+console.log(msg); //Genera error
+*/
+
+import {suma} from "./suma.js";
+
+console.log(suma(5,6));
---
+++ Encuentro_24_Modulos/scripts/suma.js
@@ -0,0 +1,10 @@
+/**
+ * Retorna la suma de dos números.
+ *
+ * @param {number} a
+ * @param {number} b
+ 
+function suma (a, b){
+    return a + b;
+}*/
+export let suma = (a, b) => a + b;
