e3331b2aea5b738d515d8c9527ce4d5360794fed
DWFEonline3 <dwfe.online.3@gmail.com>
2021-03-27 13:54:31 -0500

    Taskineitor V3.0

---
+++ Taskineitor/V3.0/src/components/App.jsx
@@ -0,0 +1,22 @@
+import '../css/App.css';
+import Tarea from './Tarea';
+import {tareas} from '../data/tareas.js'
+export default function App() {
+  return (
+    <div className="AppTaskineitor" >
+      {tareas.map(
+        tarea=>{
+          return(
+            <Tarea 
+              id={tarea.id}
+              titulo={tarea.titulo}
+              completada={tarea.completada}
+              fechaCreada={tarea.fechaCreada}
+              categoria={tarea.categoria}
+            />
+          );
+        }
+      )}
+    </div>
+  );
+}
---
+++ Taskineitor/V3.0/src/components/Tarea.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import '../css/Tarea.css';
+
+export default function Tarea(props) {
+  let tituloClick = (e)=>console.log(e.target.textContent);
+  let fechaClick = (e)=>console.log(e.target.textContent);
+  let eliminarClick = (e)=>alert("¿Deseas borrar la tarea " + e.target.alt + " - Hoy "+ new Date().toLocaleDateString("en-US") +"?");
+  const [check, cambiarCheck] = useState(props.completada);
+  let clickCheck=(e)=>{
+    cambiarCheck(!check);
+  }
+  return (
+    <div className="task" key={props.id }>
+      <div className="left">
+        <span className="titulo" onClick={tituloClick}>{props.titulo}</span>
+        <span className="fecha fecha_click" onClick={fechaClick}>{props.fechaCreada}</span>
+        <span className="fecha fecha_dias">{calcularFecha(props.fechaCreada.split('/'))}</span>
+        <span className="cat_desc">Categoria:</span>
+        <span className="categoria">{props.categoria}</span>
+      </div>
+      <div className="right">
+        <input type="checkbox" defaultChecked={check} onClick={clickCheck}/>
+        <img onClick={eliminarClick} alt={props.titulo} src="https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/trash-512.png"/>
+      </div>
+    </div>
+  );
+}
+
+function calcularFecha(date){
+  const fecha = new Date(date[2], date[1], date[0])
+  const hoy = new Date();
+  let diferencia = hoy.getTime() - fecha.getTime();
+  let diferenciaDias = Math.floor(diferencia / (1000 * 3600 * 24));
+  return diferenciaDias>0?`  - Hace ${diferenciaDias} días`:' - Hoy';
+}
